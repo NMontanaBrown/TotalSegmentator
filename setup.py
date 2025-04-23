99b0ba9bb4440bcd30ee39fe3952b604e025e8ab
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2025-04-23 15:50:43 -0700

    enh: point nnunet install to patched nnunet_cust for weight loading

--- setup.py
+++ setup.py
@@ -33,9 +33,7 @@ setup(name='TotalSegmentator',
             'xvfbwrapper',
             'fury',
             'batchgenerators==0.21',
-            # This does not work if want to upload to pypi
-            # 'nnunet @ git+https://github.com/wasserth/nnUNet_cust@working_2022_03_18#egg=nnUNet'
-            'nnunet-customized==1.2',
+            'nnunet @ git+https://github.com/NMontanaBrown/nnUNet_cust.git',
             f'requests{requests_version}',
             'rt_utils'
         ],

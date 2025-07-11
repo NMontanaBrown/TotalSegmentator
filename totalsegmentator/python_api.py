0a027fe7eb6fd358fbcec73e3a954655f2bf210d
NMontanaBrown <nina.brown.15@ucl.ac.uk>
2025-07-11 14:52:07 +0000

    chore: remove logging

--- totalsegmentator/python_api.py
+++ totalsegmentator/python_api.py
@@ -209,10 +209,6 @@ def totalsegmentator(input, output, ml=False, nr_thr_resamp=1, nr_thr_saving=6,
     seg = seg_img.get_fdata().astype(np.uint8)
 
     config = setup_totalseg()
-    increase_prediction_counter()
-    send_usage_stats(config, {"task": task, "fast": fast, "preview": preview,
-                              "multilabel": ml, "roi_subset": roi_subset, 
-                              "statistics": statistics, "radiomics": radiomics})
 
     if statistics:
         if not quiet: print("Calculating statistics...")
